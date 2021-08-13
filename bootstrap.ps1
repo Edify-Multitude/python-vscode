1e71e77fabc19fad080d10deb669cb38061a293e
Edify-Multitude <58188859+Edify-Multitude@users.noreply.github.com>
2021-08-13 19:15:57 +0200

    fix incorrect directory

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,3 @@
-New-Item -Path .vscode -ea SilentlyContinue | Out-Null
+New-Item -Path .vscode -ItemType Directory -ea SilentlyContinue | Out-Null
 irm https://raw.githubusercontent.com/Edify-Multitude/python-vscode/main/settings.json `
     -outfile .vscode/settings.json
